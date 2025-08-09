434b3a91cf662f2d9394daf32a7c5b71c3ff1dfe
ImYur <chias-call.0a@icloud.com>
2025-08-09 10:31:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,6 @@ setup_handlers(bot)
 
 # стабільний запуск
 bot.infinity_polling(skip_pending=True, timeout=60, long_polling_timeout=60)
+@bot.message_handler(commands=['getchatid'])
+def get_chat_id(message):
+    bot.reply_to(message, f"chat.id = {message.chat.id}")
