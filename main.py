29f975e399c4137338a5c5f2715163331ae93f2a
ImYur <chias-call.0a@icloud.com>
2025-08-09 10:10:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-
-from telebot import TeleBot
 import os
-
+from telebot import TeleBot
 from handlers import setup_handlers
 
 TOKEN = os.getenv("BOT_TOKEN")
-bot = TeleBot(TOKEN)
+if not TOKEN:
+    raise RuntimeError("ENV BOT_TOKEN is missing")
+
+bot = TeleBot(TOKEN, parse_mode="Markdown")
 
 setup_handlers(bot)
 
-bot.infinity_polling()
+# стабільний запуск
+bot.infinity_polling(skip_pending=True, timeout=60, long_polling_timeout=60)
